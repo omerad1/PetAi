73c02ae3c5772c6429b7410f57f405d3b57c8a9a
omerad1 <127669568+omerad1@users.noreply.github.com>
2024-06-22 14:14:06 +0300

    starting project

---
+++ pet-ai/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p dir="ltr">שלום, בואו להכיר את חיית המחמד הבאה שלכם </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
